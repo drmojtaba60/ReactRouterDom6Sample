fbc92f6d04fced9685ca679cc36e9a80917ed543
drmojtaba60 <mojtaba.shagi@gmail.com>
2023-10-28 00:35:10 +0330

    submenu 14020805

--- src/Layouts/sidebar.jsx
+++ src/Layouts/sidebar.jsx
@@ -1,13 +1,22 @@
 import logo from '@/assets/img/logos/csis.png';
+import { useState } from 'react';
 const SideBar =(props)=>
 {
-  return <div className="sidebar inactive">
+  const [inactive,setInterval]=useState(false);
+
+  return <div className={`sidebar ${inactive ? "inactive" : ""}`}>
           <div className="top-section">
               <div className="logo">
                 <img src={logo} />
               </div>
-              <div className='toggle-menu-btn'>
-               <i className="bi bi-arrow-left-square-fill"></i>
+              <div className='toggle-menu-btn' 
+                onClick={()=>setInterval(!inactive)} >
+                  {
+                   inactive 
+                        ? <i className="bi bi-arrow-left-square-fill"></i>
+                        :<i className="bi bi-arrow-right-square-fill"></i>
+                  }
+               
               </div>
           </div>
           <div className="search-controler">
@@ -18,6 +27,41 @@ const SideBar =(props)=>
           </div>
 
           <div className="divider"></div>
+
+          <div className="main-menu">
+              <ul>
+                <li>
+                  <a href="" className="menu-item">
+                    <div className="menu-icon">
+                      <i className="bi bi-speedometer2"></i>
+                    </div>
+                    <span>Dashboard</span>
+                  </a>
+                </li>
+
+                <li>
+                  <a href="" className="menu-item">
+                    <div className="menu-icon">
+                      <i className="bi bi-newspaper"></i>
+                    </div>
+                    <span>Contents</span>
+                  </a>
+                  <ul className="sub-menu">
+                    <li >
+                      <a href="">Course</a>
+                    </li>
+                    <li >
+                      <a href="">Videos</a>
+                    </li>
+                  </ul>
+                </li>
+
+              </ul>      
+          </div>
+
+          <div>
+
+          </div>
          </div>;
 }
 
